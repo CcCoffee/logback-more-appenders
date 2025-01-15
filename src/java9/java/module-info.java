aa3238f440c2ea645bef0817781b27f964135710
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2025-01-15 10:19:24 +0800

    only keep fluentd appender

--- src/java9/java/module-info.java
+++ src/java9/java/module-info.java
@@ -4,14 +4,4 @@ open module org.slf4j {
   requires static logback.classic;
   requires static logback.access;
   requires static fluent.logger;
-  requires static fluency.core;
-  requires static fluency.fluentd;
-  requires static aws.java.sdk.core;
-  requires static aws.java.sdk.logs;
-  requires static aws.java.sdk.kinesis;
-  requires static software.amazon.awssdk.core;
-  requires static software.amazon.awssdk.auth;
-  requires static software.amazon.awssdk.regions;
-  requires static software.amazon.awssdk.services.kinesis;
-  requires static software.amazon.awssdk.services.cloudwatchlogs;
 }
