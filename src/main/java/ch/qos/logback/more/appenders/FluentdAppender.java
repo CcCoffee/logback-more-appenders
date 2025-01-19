762bf81f773ba86209830ab51c46541e6bb8560e
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2025-01-19 11:11:01 +0800

    合并Appender

--- src/main/java/ch/qos/logback/more/appenders/FluentdAppender.java
+++ src/main/java/ch/qos/logback/more/appenders/FluentdAppender.java
@@ -14,8 +14,9 @@ import ch.qos.logback.core.AppenderBase;
 import ch.qos.logback.core.Layout;
 import ch.qos.logback.core.encoder.Encoder;
 import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+import org.fluentd.logger.FluentLogger;
 
-public abstract class FluentdAppenderBase<E> extends AppenderBase<E> {
+public class FluentdAppender<E> extends AppenderBase<E> {
     private static final String DATA_MESSAGE = "message";
     private static final String DATA_LOGGER = "logger";
     private static final String DATA_THREAD = "thread";
@@ -27,6 +28,41 @@ public abstract class FluentdAppenderBase<E> extends AppenderBase<E> {
     protected Map<String, String> additionalFields;
     private String messageFieldKeyName = DATA_MESSAGE;
     private List<String> ignoredFields;
+    private FluentLogger fluentLogger;
+    private Integer timeout;
+    private Integer bufferCapacity;
+    private String label;
+
+    @Override
+    public void start() {
+        fluentLogger = FluentLogger.getLogger(getLabel() != null ? getTag() : null, getRemoteHost(), getPort(),
+                getTimeout(), getBufferCapacity());
+        super.start();
+    }
+
+    @Override
+    public void stop() {
+        try {
+            super.stop();
+        } finally {
+            try {
+                FluentLogger.closeAll();
+            } catch (Exception e) {
+                // pass
+            }
+        }
+    }
+
+    @Override
+    protected void append(E event) {
+        Map<String, Object> data = createData(event);
+
+        if (isUseEventTime()) {
+            fluentLogger.log(getLabel() == null ? getTag() : getLabel(), data, System.currentTimeMillis() / 1000);
+        } else {
+            fluentLogger.log(getLabel() == null ? getTag() : getLabel(), data);
+        }
+    }
 
     protected Map<String, Object> createData(E event) {
         Map<String, Object> data = new HashMap<String, Object>();
@@ -43,9 +79,7 @@ public abstract class FluentdAppenderBase<E> extends AppenderBase<E> {
             if (loggingEvent.getThrowableProxy() != null) {
                 data.put(DATA_THROWABLE, ThrowableProxyUtil.asString(loggingEvent.getThrowableProxy()));
             }
-            for (Map.Entry<String, String> entry : loggingEvent.getMDCPropertyMap().entrySet()) {
-                data.put(entry.getKey(), entry.getValue());
-            }
+            data.putAll(loggingEvent.getMDCPropertyMap());
         } else {
             data.put(messageFieldKeyName, encoder != null ? encoder.encode(event) : event.toString());
         }
@@ -55,7 +89,7 @@ public abstract class FluentdAppenderBase<E> extends AppenderBase<E> {
         }
 
         if(ignoredFields != null) {
-            ignoredFields.stream().forEach(data::remove);
+            ignoredFields.forEach(data::remove);
         }
 
         return data;
@@ -158,5 +192,29 @@ public abstract class FluentdAppenderBase<E> extends AppenderBase<E> {
 
     public void setMessageFieldKeyName(String messageFieldKeyName) {
         this.messageFieldKeyName = messageFieldKeyName;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setTimeout(Integer timeout) {
+        this.timeout = timeout;
+    }
+
+    public int getTimeout() {
+        return timeout != null ? timeout : 1000;
+    }
+
+    public void setBufferCapacity(Integer bufferCapacity) {
+        this.bufferCapacity = bufferCapacity;
+    }
+
+    public int getBufferCapacity() {
+        return bufferCapacity != null ? bufferCapacity : 16777216;
     }
 }
